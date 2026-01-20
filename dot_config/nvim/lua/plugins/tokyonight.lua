e0e061b00f2783a4096ee2470a51f822d9ec4a18
tak-seta <135773238+seta-takumi@users.noreply.github.com>
2026-01-20 20:41:53 +0900

    Add .config/nvim/lua/plugins/tokyonight.lua

---
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    enable = false,
+    lazy = false, -- If it is not loaded at startup, it will not be added to the ui list.
+    opts = {
+      style = "moon",
+      transparent = true,
+    },
+  },
+}
